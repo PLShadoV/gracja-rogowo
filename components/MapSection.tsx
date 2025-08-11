fbac0b3eebd68869c3533357a043aff75f9f1fa7
PLShadoV <plshadov@gmail.com>
2025-08-11 22:21:39 +0200

    1234

--- components/MapSection.tsx
+++
@@ -1,7 +0,0 @@
-export default function MapSection(){
-  return (
-    <div id="map" className="overflow-hidden rounded-2xl border" style={{ borderColor: "#e6e8ef" }}>
-      <iframe title="Mapa" src="https://maps.google.com/maps?q=Rogowo%20Poland&t=&z=12&ie=UTF8&iwloc=&output=embed" className="h-80 w-full" loading="lazy" />
-    </div>
-  );
-}
